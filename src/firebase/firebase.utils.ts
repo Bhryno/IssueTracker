262b7a1329595b2c540ab53726cfab4cbe22f71f
HairConditioner <bryanrplee@gmail.com>
2023-05-05 09:14:28 +1000

    Material UI engine

--- src/firebase/firebase.utils.ts
+++ src/firebase/firebase.utils.ts
@@ -4,54 +4,54 @@ import "firebase/auth";
 import "firebase/storage";
 
 const config = {
-  apiKey: "AIzaSyBcSu1C4Qdud_a52NOWikVSOj0njkwODpc",
-  authDomain: "bugtrail-v2.firebaseapp.com",
-  databaseURL: "https://bugtrail-v2.firebaseio.com",
-  projectId: "bugtrail-v2",
-  storageBucket: "bugtrail-v2.appspot.com",
-  messagingSenderId: "723885045033",
-  appId: "1:723885045033:web:4eeef384d51bf7c025b338",
-  measurementId: "G-VH49TBGZ9N",
+    apiKey: "AIzaSyBcSu1C4Qdud_a52NOWikVSOj0njkwODpc",
+    authDomain: "bugtrail-v2.firebaseapp.com",
+    databaseURL: "https://bugtrail-v2.firebaseio.com",
+    projectId: "bugtrail-v2",
+    storageBucket: "bugtrail-v2.appspot.com",
+    messagingSenderId: "723885045033",
+    appId: "1:723885045033:web:4eeef384d51bf7c025b338",
+    measurementId: "G-VH49TBGZ9N",
 };
 
 firebase.initializeApp(config);
 
 export const createUserProfileDocument = async (
-  userAuth: firebase.User | null,
-  additionalData: any
+    userAuth: firebase.User | null,
+    additionalData: any
 ) => {
-  if (!userAuth) return;
+    if (!userAuth) return;
 
-  const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
 
-  const snapShot = await userRef.get();
+    const snapShot = await userRef.get();
 
-  if (!snapShot.exists) {
-    const { displayName, email } = userAuth;
-    const createdAt = new Date();
-    try {
-      await userRef.set({
-        displayName,
-        email,
-        createdAt,
-        myTickets: [""],
-        ...additionalData,
-      });
-    } catch (error: any) {
-      console.log("error creating user", error);
+    if (!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                myTickets: [""],
+                ...additionalData,
+            });
+        } catch (error: any) {
+            console.log("error creating user", error);
+        }
     }
-  }
 
-  return userRef;
+    return userRef;
 };
 
 export const getCurrentUser = () => {
-  return new Promise((resolve, reject) => {
-    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
-      unsubscribe();
-      resolve(userAuth);
-    }, reject);
-  });
+    return new Promise((resolve, reject) => {
+        const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+            unsubscribe();
+            resolve(userAuth);
+        }, reject);
+    });
 };
 
 export const auth = firebase.auth();
